d7bf07e4e09f44de9168db083f6350b1fe89de64
grimsleyroboticsapollo <grimsleyroboticsapollo@gmail.com>
2018-02-14 18:34:45 -0500

    Created Joystick Helper

---
+++ src/org/usfirst/frc/team3215/robot/JoystickHelper.java
@@ -0,0 +1,126 @@
+package org.usfirst.frc.team3215.robot;
+
+import org.usfirst.frc.team3215.robot.config.RobotHardware;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+public class JoystickHelper {
+
+	private RobotHardware r;
+
+	public JoystickHelper(RobotHardware r) {
+		this.r = r;
+	}
+	
+	private double targetDirection;
+	private double targetAngle;
+	private double driveSpeed;
+	private double turnSpeed;
+	private final static double JOY_THRESHOLD = 0.1;
+	private final static double SLOW_FACTOR = 3.;
+	
+	private final static double RAD_TO_DEGREES = 180. / Math.PI;
+
+    /**
+     * Given a joystick position, calculates the angle where it is pointing at.
+     *
+     * @param joyX The joystick x-axis position. 0 = neutral, -1.0 is left, and +1.0 is right
+     * @param joyY The joystick y-axis position. 0 = neutral, -1.0 is down, and +1.0 is up
+     * @return Angle in degrees, between 0 (including) and 360 (excluding) degrees. 0 degrees is
+     * considered "forward", 90 is "left", and 270 is "right".
+     * <p>
+     * Usage exmample:
+     * <p>
+     * double angle = JoystickHelper.getAngle(leftX, leftY);
+     */
+    public double getAngle(double joyX, double joyY) {
+
+        double joyXabs = Math.abs(joyX);
+        boolean joyXpositive = (joyX > 0);
+        double joyYabs = Math.abs(joyY);
+        boolean joyYpositive = (joyY > 0);
+
+        if (joyY == 0) {
+            if (joyXpositive) {
+                return 270.;
+            } else {
+                return 90.;
+            }
+        }
+
+        if (joyX == 0) {
+            if (joyYpositive) {
+                return 0.;
+            } else {
+                return 180.;
+            }
+        }
+
+        if (joyXpositive) {
+            // 3rd or 4th quadrant
+
+            if (joyYpositive) {
+                // 4th quadrant
+
+                return 270. + Math.atan(joyYabs / joyXabs) * RAD_TO_DEGREES;
+
+            } else {
+
+                return 180. + Math.atan(joyXabs / joyYabs) * RAD_TO_DEGREES;
+
+            }
+
+
+        } else {
+            // 1st or 2nd quadrant
+
+            if (joyYpositive) {
+                // 1st quadrant
+
+                return 0. + Math.atan(joyXabs / joyYabs) * RAD_TO_DEGREES;
+
+            } else {
+
+                return 90. + Math.atan(joyYabs / joyXabs) * RAD_TO_DEGREES;
+
+            }
+
+        }
+
+
+    }
+	
+	public void read() {
+		
+		double joy0x = r.joystick0().getX(Hand.kLeft);
+		double joy0y = r.joystick0().getY(Hand.kLeft);
+		double joy0angle = getAngle(joy0x, joy0y);
+		double joy0speed = Math.sqrt(joy0x * joy0x + joy0y * joy0y);
+		double joy1x = r.joystick1().getX(Hand.kLeft);
+		double joy1y = r.joystick1().getY(Hand.kLeft);
+		double joy1angle = getAngle(joy1x, joy1y);
+		double joy1speed = Math.sqrt(joy1x * joy1x + joy1y * joy1y);
+		
+		if(joy0speed > JOY_THRESHOLD) {
+			
+			targetDirection = joy0angle;
+			driveSpeed = joy0speed;
+		}else if(r.joystick0().getPOV() >= 0) {
+			
+			double povAngle = 360. - r.joystick0().getPOV();
+			targetAngle = r.imu().getHeadingBestTwoOfThree() + povAngle;
+			driveSpeed = 1.;
+		}else if(joy1speed > JOY_THRESHOLD){
+			
+			targetAngle = r.imu().getHeadingBestTwoOfThree() + joy1angle;
+			driveSpeed = joy1speed / SLOW_FACTOR;
+		}else {
+			
+			targetAngle = r.imu().getHeadingBestTwoOfThree();
+			driveSpeed = 0;
+		}
+		
+		
+	}
+	
+}
